9a089d1210a60a7fe98febf823e99da4fd6a6941
yymmhh <yymmhh@126.com>
2018-06-07 09:01:34 +0800

    123123123123

--- index.sh
+++ index.sh
@@ -15,11 +15,11 @@ read commit
  echo $commit;
 
 
- echo -e “\033[33m 正在提交注释 \033[0m” 
+ echo -e "\033[33m 正在提交注释 \033[0m" ;
  git commit -m $commit;
 
- echo -e “\033[33m 正在提交代码 \033[0m” 
+ echo -e "\033[33m 正在提交代码 \033[0m" ;
  git push;
 
-　echo -e “\033[32m 提交代码成功! \033[0m” 
+　echo -e "\033[32m 提交代码成功! \033[0m" ;
 
