3e38e5ac684c81afc1d9e121ec16fc2ab3982744
yymmhh <yymmhh@126.com>
2018-06-07 09:00:25 +0800

    123123123123213123

--- index.sh
+++ index.sh
@@ -1,15 +1,25 @@
+echo "开始拉取代码"
 git pull;
-git add . ;
+echo "拉取完成"
 
-echo "请输入代码的注释:"
+
+
+echo "开始添加代码";
+git add . ;
+echo "添加完成"
+
+
+echo -e "\033[47;34m 请输入代码注释 \033[0m" ; 
 read commit
 
  echo $commit;
 
 
+ echo -e “\033[33m 正在提交注释 \033[0m” 
  git commit -m $commit;
 
+ echo -e “\033[33m 正在提交代码 \033[0m” 
  git push;
 
- echo "代码提交成功!";
+　echo -e “\033[32m 提交代码成功! \033[0m” 
 
